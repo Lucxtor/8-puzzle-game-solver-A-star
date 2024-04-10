252868591c26343d6943521e0256a9ee3fc01115
Luis Felipe Fabiane <luisffabiane@gmail.com>
2024-04-10 13:08:13 -0300

    Escreve função generateChildren e cria um arquivo de testes para uso sem a lógica principal

--- states.py
+++ states.py
@@ -4,8 +4,12 @@ class State():
         self.cost = 0
         self.parent = parent
 
+    def __str__(self):
+        state_str = '\n'.join([str(self.state[i:i+3]) for i in range(0, 9, 3)])
+        return f"Estado:\n{state_str}\nCusto: {self.cost}"
+
     def isSolution(self):
-        if self.state == ['1', '2', '3', '5', '4', '6', '7', '8', '0']:
+        if self.state == ['1', '2', '3', '4', '5', '6', '7', '8', '0']:
             return True
         return False
 
@@ -13,4 +17,61 @@ class State():
         self.cost += 1 # TODO apply heuristic
 
     def generateChildren(self):
-        pass # TODO generate based on possible moves and return a list of States with current state as parent
+
+            # Index do tabuleiro
+            # 0 1 2
+            # 3 4 5
+            # 6 7 8
+
+            # Se index 0 direita ou baixo
+            # Se index 1 direita, esquerda ou baixo
+            # Se index 2 esquerda ou baixo
+            # Se index 3 cima, direita ou baixo
+            # Se index 4 cima, direita, esquerda ou baixo
+            # Se index 5 cima, esquerda ou baixo 
+            # Se Index 6 cima ou direita 
+            # Se index 7 cima, direita ou esquerda
+            # Se index 8 cima ou esquerda
+        
+            # Direita = index + 1
+            # Esquerda = index - 1
+            # Cima = index - 3
+            # Baixo = index + 3
+
+        index = self.state.index('0')
+        children = []
+        aux = ''
+
+        if index not in [2, 5, 8]: # Movimento para a direita
+            stateCopy = self.state.copy()
+            aux = stateCopy[index+1]
+            stateCopy[index+1] = '0'
+            stateCopy[index] = aux
+
+            children.append(State(stateCopy, self))
+        
+        if index not in [0, 3, 6]: # Movimento para a esquerda
+            stateCopy = self.state.copy()
+            aux = stateCopy[index-1]
+            stateCopy[index-1] = '0'
+            stateCopy[index] = aux
+
+            children.append(State(stateCopy, self))
+
+        if index not in [6, 7, 8]: # Movimento para baixo
+            stateCopy = self.state.copy()
+            aux = stateCopy[index+3]
+            stateCopy[index+3] = '0'
+            stateCopy[index] = aux
+
+            children.append(State(stateCopy, self))
+        
+        if index not in [0, 1, 2]: # Movimento para cima
+            stateCopy = self.state.copy()
+            aux = stateCopy[index-3]
+            stateCopy[index-3] = '0'
+            stateCopy[index] = aux
+
+            children.append(State(stateCopy, self))
+
+        return children
---
+++ teste.py
@@ -0,0 +1,7 @@
+from states import State
+
+state = State(['1', '2', '3', '4', '5', '6', '7', '0', '8'])
+
+print(state)
+
+state.generateChildren()
